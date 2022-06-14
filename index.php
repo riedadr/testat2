0c37b952b6decb69e8b31e8757e7fd7fe13d291e
friedrich <riedadr@gmail.com>
2022-06-14 12:32:35 +0200

    favicon + fragen 600px

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="icon" href="favicon.gif" type="image/gif" >
 	<link rel="stylesheet" href="/styles/dashboard.css" />
 	<title>Umfrage</title>
 </head>
