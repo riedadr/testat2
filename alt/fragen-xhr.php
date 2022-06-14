852507b22a3a35503fc82411b97c18479d397de7
karsten <riedadr@gmail.com>
2022-06-14 23:53:09 +0200

    fragen-mini.html gelöscht

--- alt/fragen-xhr.php
+++ alt/fragen-xhr.php
@@ -39,7 +39,7 @@ if ($json != null) {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="fragen-mini.css" />
+    <link rel="stylesheet" href="fragen-xhr.css" />
     <title>Umfrage</title>
 </head>
 
